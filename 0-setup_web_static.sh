297475fa5e1fdfe080e5494d735bca79c0554eee
joan cruz <joancruz95@gmail.com>
2020-01-11 02:31:19 +0000

    Edit syntax errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get install ngnix
-ufw allow 'Ngnix HTTP'
+sudo apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/{releases/test,shared}
-cat > /data/web_static/shared/releases/test/index.html << 'EOF'
+cat > /data/web_static/releases/test/index.html << 'EOF'
 <html>
   <head>
   </head>
@@ -13,7 +13,7 @@ cat > /data/web_static/shared/releases/test/index.html << 'EOF'
   </body>
 </html>
 EOF
-ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '{ listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/ }' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
