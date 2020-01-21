f3c731bd242fc7cdec2b86bc8dab5957936d1bf5
joan cruz <joancruz95@gmail.com>
2020-01-21 02:31:56 +0000

    Add comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+"""Flask web application"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def helloep8():
+@app.route("/", strict_slashes=False)
+def HelloHBNB():
+    "Route to display text"
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
+    """Listening route and port"""
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
