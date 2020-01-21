bec1b0e8d429db269477f34c23831ac454294e3b
joan cruz <joancruz95@gmail.com>
2020-01-21 02:32:49 +0000

    Add comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def HelloHBNB():
+    "Route to display text"
     return "Hello HBNB!"
 
 
-@app.route("/hbnb")
+@app.route("/hbnb", strict_slashes=False)
 def HBNB():
+    "Route to display text"
     return "HBNB"
 
 
 if __name__ == '__main__':
+    """Listening route and port"""
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
