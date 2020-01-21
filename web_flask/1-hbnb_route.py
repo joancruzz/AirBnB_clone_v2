cf50408cea4b6e1cb550745b1c829e16a11e383b
joan cruz <joancruz95@gmail.com>
2020-01-21 01:10:43 +0000

    Add route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def HelloHBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def HBNB():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
