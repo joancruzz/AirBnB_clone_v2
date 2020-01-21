2d0e486c6ff3cb71e0be0fec1b9619c8f69464b0
joan cruz <joancruz95@gmail.com>
2020-01-21 22:41:16 +0000

    Pass variable between flask and html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def NumberTemplate(n):
     """Route to display html page if n is an integer"""
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
