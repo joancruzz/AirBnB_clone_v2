223b61c9e20d84a22f4088670206b40c8b14774a
joan cruz <joancruz95@gmail.com>
2020-01-21 02:38:49 +0000

    Edit comments

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def HelloHBNB():
-    "Route to display text"
+    """Route to display text"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
-    "Route to display text"
+    """Route to display text"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def CText(text):
-    "Route to display text"
+    """Route to display text"""
     return "C {}".format(text.replace("_", " "))
 
 
