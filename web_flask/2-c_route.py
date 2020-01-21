f0369819bfa13cf58858851d106cf7b434c99748
joan cruz <joancruz95@gmail.com>
2020-01-21 02:36:43 +0000

    Edit comments

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
+"""Flask web application"""
 import re
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def HelloHBNB():
+    "Route to display text"
     return "Hello HBNB!"
 
 
-@app.route("/hbnb")
+@app.route("/hbnb", strict_slashes=False)
 def HBNB():
+    "Route to display text"
     return "HBNB"
 
 
-@app.route("/c/<text>")
-def Ctext(text):
+@app.route("/c/<text>", strict_slashes=False)
+def CText(text):
+    "Route to display text"
     return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
+    """Listening route and port"""
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
